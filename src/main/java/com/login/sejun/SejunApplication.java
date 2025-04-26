b2283091388d2297945de85e38176a23617d2fda
Sejun Park <pkt369@naver.com>
2025-04-26 15:59:00 -0400

    init

---
+++ src/main/java/com/login/sejun/SejunApplication.java
@@ -0,0 +1,13 @@
+package com.login.sejun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/login/sejun/SejunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.sejun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
